d43c48d9e42afae21627c1990f1d7b70a10c2cb2
Ablaiuly Dinmukhammed <144629107+midsti-dev@users.noreply.github.com>
2024-11-28 19:31:13 +0000

    upd1

--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,45 @@
-public class Order {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+class Order {
+    private List<Pizza> pizzas = new ArrayList<>();
+    private String deliveryType;
+
+    public void addPizza(Pizza pizza) {
+        pizzas.add(pizza);
+    }
+
+    public void setDeliveryType(String deliveryType) {
+        this.deliveryType = deliveryType;
+    }
+
+    public void showOrder() {
+        System.out.println("Заказ включает:");
+        for (Pizza pizza : pizzas) {
+            pizza.prepare();
+        }
+        System.out.println("Тип доставки: " + deliveryType);
+    }
+}
+
+class OrderBuilder {
+    private Order order;
+
+    public OrderBuilder() {
+        order = new Order();
+    }
+
+    public OrderBuilder addPizza(Pizza pizza) {
+        order.addPizza(pizza);
+        return this;
+    }
+
+    public OrderBuilder setDeliveryType(String deliveryType) {
+        order.setDeliveryType(deliveryType);
+        return this;
+    }
+
+    public Order build() {
+        return order;
+    }
 }
